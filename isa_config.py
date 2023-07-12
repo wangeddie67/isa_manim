a36fc2a7fa6ccef901f735cfb2088be5e796c388
Junshi Wang <junwan02@a015939.shanghai.arm.com>
2023-07-12 11:23:11 +0800

    Add comments.

--- isa_config.py
+++ isa_config.py
@@ -5,6 +5,9 @@ ISA configuration structure.
 isa_config = {
     "scene_ratio": (1/8),   # scene width / bit. Default 1.0 means 8 bit.
 }
+"""
+Configuration structure to pass arguments of ISA. For example, element size and vector length.
+"""
 
 def get_scene_ratio() -> float:
     """
